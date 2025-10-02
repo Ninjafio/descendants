845533dbf57961d028434fde111efc39fa244d2e
mozgotrax <kitten181007@gmail.com>
2025-10-02 19:57:27 +0500

    сделал маштабирование и drag and drop но есть баг

--- src/components/radialTree/RadialTree.tsx
+++ src/components/radialTree/RadialTree.tsx
@@ -1,10 +1,56 @@
-import React from "react";
+'use client'
+import React, { useState, useEffect, useRef } from 'react';
 import "./RadialTree.scss";
 
 const RadialTree = () => {
+  const [scaleFactor, setScaleFactor] = useState(1);
+  const [positionX , setPositionX] = useState(0);
+  const [positionY , setPositionY] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const startPosition = useRef({ x: null, y: null });
+  const containerRef = useRef(null);
+  useEffect(() => {
+    const handleWheel = (event:any) => {
+      if (event.deltaY > 0 && scaleFactor >= 0.5) {
+        setScaleFactor((prevScale) => prevScale - 0.1);
+      } else if (event.deltaY < 0 && scaleFactor <= 2) {
+        setScaleFactor((prevScale) => prevScale + 0.1);
+      }
+    };
+    window.addEventListener("wheel", handleWheel);
+    return () => {
+      window.removeEventListener("wheel", handleWheel);
+    };
+  }, [scaleFactor]);
+   // Обработка начала перетаскивания
+  const onMouseDown = (event:any) => {
+    setIsDragging(true);
+    startPosition.current.x = event.clientX;
+    startPosition.current.y = event.clientY;
+  };
+
+  // Обработка завершения перетаскивания
+  const onMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  // Обработка перемещения мыши
+  const onMouseMove = (event:any) => {
+    if (!isDragging || !startPosition.current.x || !startPosition.current.y) return;
+
+    const deltaX = event.clientX - startPosition.current.x;
+    const deltaY = event.clientY - startPosition.current.y;
+
+    setPositionX(positionX + deltaX / scaleFactor);
+    setPositionY(positionY + deltaY / scaleFactor);
+
+    startPosition.current.x = event.clientX;
+    startPosition.current.y = event.clientY;
+  };
+
   return (
-    <div>
-      <div className="RadialTree"></div>
+    <div style={{  transform: `translate(${positionX}px, ${positionY}px) scale(${scaleFactor})`, transformOrigin: 'top left' }} className="container">
+      <div className="RadialTree"  ref={containerRef} onMouseDown={onMouseDown} onMouseUp={onMouseUp} onMouseMove={onMouseMove}></div>
       <div className="cell1 l"></div>
       <div className="cell1 r"></div>
 
